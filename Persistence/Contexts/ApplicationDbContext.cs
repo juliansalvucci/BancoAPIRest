17d69b52c0202be2132304ea47ce43f5147f39b1
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-02-20 19:32:57 -0300

    repository v1.0

---
+++ Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Application.Interfaces;
+using Domain.Common;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IDateTimeService _dateTime;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IDateTimeService dateTime) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            _dateTime = dateTime;
+        }
+        public DbSet<Cliente> Clientes { get; set;}
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationTaken = new CancellationToken())
+        {
+            foreach(var entry in ChangeTracker.Entries<AuditableBaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.Created = DateTime.UtcNow;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModified = DateTime.UtcNow;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationTaken);
+        }
+    }
+}
