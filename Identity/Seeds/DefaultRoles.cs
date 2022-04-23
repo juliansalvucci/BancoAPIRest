f7df3332eac391a4bb1223cf637009aa025c587a
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-04-22 22:32:44 -0300

    JWT 21

---
+++ Identity/Seeds/DefaultRoles.cs
@@ -0,0 +1,15 @@
+﻿
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Identity.Seeds
+{
+    public static class DefaultRoles
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> role)
+        {
+            //TO DO
+        }
+    }
+}
