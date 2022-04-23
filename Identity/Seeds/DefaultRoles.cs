be4493576788202a1ede247f6b09d787d2135b3c
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-04-23 01:31:02 -0300

    SEEDS JWT

--- Identity/Seeds/DefaultRoles.cs
+++ Identity/Seeds/DefaultRoles.cs
@@ -1,4 +1,5 @@
 ﻿
+using Application.Enums;
 using Identity.Models;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
@@ -7,9 +8,10 @@ namespace Identity.Seeds
 {
     public static class DefaultRoles
     {
-        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> role)
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            //TO DO
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Basic.ToString()));
         }
     }
 }
