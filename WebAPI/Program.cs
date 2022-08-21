3d7fcf8a2bfc7838a9dbb8088ecdc3f99eabeebb
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-08-20 21:15:18 -0300

    jwt

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,14 +1,37 @@
+using Identity.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Identity.Seeds;
+using System;
 
 namespace WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    await DefaultRoles.SeedAsync(userManager, roleManager);
+                    await DefaultAdminUser.SeedAsync(userManager, roleManager);
+                    await DefaultBasicUser.SeedAsync(userManager, roleManager);
+
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
